e7606c8dfa9750bc6e0c523bb9f70afdd75b4da2
lenka783 <lenka.smitalova@gmail.com>
2018-06-09 02:31:14 +0200

    ReservationDateRangeConstraint added

---
+++ ARS-persistence/src/main/java/cz/muni/fi/pv243/ars/persistence/validation/ReservationDateRangeConstraint.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pv243.ars.persistence.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {ReservationDateRangeValidator.class})
+public @interface ReservationDateRangeConstraint {
+
+    String message () default "Validation of Reservation failed.";
+    Class<?>[] groups () default {};
+    Class<? extends Payload>[] payload () default {};
+}
